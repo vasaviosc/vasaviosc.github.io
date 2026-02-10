c5754674532b19231a875ed0a73565e9c73751a4
bhushanwho <76513310+bhushanwho@users.noreply.github.com>
2026-02-11 00:54:47 +0530

    gh pages optimizations

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  basePath: "/vosc",
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
